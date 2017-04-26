b22abeb84d8312c2dbd371561aac1d64871be65a
regocxy <regocxy@163.com>
2017-04-26 17:25:23 +0800

    commit

---
+++ sort.c
@@ -0,0 +1,25 @@
+#include "stdio.h"
+#define u8 unsigned char
+
+void ins_sort(u8 *d,u8 l)
+{
+	u8 i,j;
+	for(i=0;i<l;i++)
+	{
+		for(j=i;(j>0)&&(d[j]<d[j-1]);j--)
+		{
+
+		}
+	}
+}
+
+void q_sort(u8 *d,u8 l)
+{
+
+}
+
+int main(int argc, char const *argv[])
+{
+	printf("hello\n");
+	return 0;
+}
